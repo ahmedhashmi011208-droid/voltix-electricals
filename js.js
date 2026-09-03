dc2c98063591941297f8e9ac33cff8bcd79c0c4d
ahmedhashmi011208-droid <ahmedhashmi011208@gmail.com>
2026-09-03 09:56:07 +0500

    Update js.js

--- js.js
+++ js.js
@@ -1069,6 +1069,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
     }
 
+// MOBILE MENU
+const menuIcon = document.querySelector(".menu-icon");
+const nav = document.querySelector(".navbar nav");
 
+if (menuIcon && nav) {
+    menuIcon.addEventListener("click", function () {
+        nav.classList.toggle("show");
+    });
+}
 
 });
